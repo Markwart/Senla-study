072a58a4ee3634880ff9195110435199984c833d
matusevich_mark <markus.matusevich@gmail.com>
2019-08-26 16:22:50 +0300

    fixing

--- Reflection-Task-3/src/by/senla/cvs/module/processor/Parsing.java
+++ Reflection-Task-3/src/by/senla/cvs/module/processor/Parsing.java
@@ -17,13 +17,14 @@ public class Parsing {
 
 		strObjMap.forEach((key, strList) -> {
 
-			String[] title = strList.get(0);
-			strList.remove(0);
+			String[] title = null;
+			// String[] title = strList.get(0);
+			// strList.remove(0);
 
 			for (String[] fieldsArray : strList) {
 
 				try {
-					Object someObject = createObject(key, title, fieldsArray, strObjMap, newObjList);
+					Object someObject = createObject(key, title, fieldsArray, strObjMap, newObjList, strList);
 					if (!newObjList.contains(someObject)) {
 						newObjList.add(someObject);
 					}
@@ -37,10 +38,8 @@ public class Parsing {
 	}
 
 	private static Object createObject(String key, String[] title, String[] fieldsArray,
-			Map<String, List<String[]>> strObjMap, List<Object> newObjList)
+			Map<String, List<String[]>> strObjMap, List<Object> newObjList, List<String[]> strList)
 			throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-		
-		System.out.println(Arrays.toString(title) + " " + Arrays.toString(fieldsArray));
 
 		String className = key;
 		Class<?> someObject = Class.forName(className);
@@ -49,11 +48,22 @@ public class Parsing {
 		Object newObject = someObject.newInstance();
 
 		for (Field field : fields) {
+
+			if (Arrays.asList(fieldsArray).contains(field.getName())) {
+
+				title = fieldsArray;
+				fieldsArray = strList.get(strList.indexOf(fieldsArray) + 1);
+				strList.remove(strList.indexOf(fieldsArray));
+			}
+			
+			System.out.println(Arrays.toString(title) + " " + Arrays.toString(fieldsArray));
+
 			field.setAccessible(true);
 			if (field.isAnnotationPresent(CsvProperty.class)) {
 				CsvProperty annField = field.getAnnotation(CsvProperty.class);
 				setFieldValue(field, newObject, title, fieldsArray, annField, strObjMap, newObjList);
 			}
+
 		}
 		return newObject;
 	}
@@ -61,7 +71,7 @@ public class Parsing {
 	private static void setFieldValue(Field field, Object newObject, String[] title, String[] fieldsArray,
 			CsvProperty annField, Map<String, List<String[]>> strObjMap, List<Object> newObjList)
 			throws IllegalArgumentException, IllegalAccessException {
-
+		
 		String fieldType = field.getType().getSimpleName();
 		String fieldValue = defineValueField(field, fieldsArray, title);
 		Object convertedValue = null;
@@ -87,7 +97,7 @@ public class Parsing {
 				if (key.contains(className) & Arrays.asList(array).get(indexArray).equals(keyField)) {
 
 					try {
-						Object relatedObject = createObject(key, title, array, strObjMap, newObjList);
+						Object relatedObject = createObject(key, title, array, strObjMap, newObjList, strList);
 
 						if (!newObjList.contains(relatedObject)) {
 							newObjList.add(relatedObject);
@@ -104,9 +114,8 @@ public class Parsing {
 
 	private static String defineValueField(Field field, String[] fieldsArray, String[] title) {
 		String fieldValue = null;
-
+		
 		for (String fieldName : title) {
-
 			if (field.getName().equals(fieldName)) {
 				int indexFieldArray = Arrays.asList(title).indexOf(fieldName);
 				fieldValue = fieldsArray[indexFieldArray];
